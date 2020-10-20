7be3f6786ee800a24b426717cf3408a77c5f2eac
Markus Felten <Markus.Felten@gmx.de>
2020-10-20 23:51:11 +0200

    fix: use esm

--- src/message.mjs
+++ src/message.mjs
@@ -1,7 +1,7 @@
-const os = require('os');
-const uuid = require('uuid-js');
-const cloneDeep = require('clone-deep');
-const merge = require('merge-light');
+import { hostname } from "os";
+import uuid from "uuid-js";
+import cloneDeep from "clone-deep";
+import merge from "merge-light";
 
 /**
  * Creates a new request message structure. It will merge the new data into a copy of the old request.
@@ -15,38 +15,38 @@ const merge = require('merge-light');
  * @param oldRequestMessage {object} The request message this step has received
  */
 export function createMessage(newData, oldRequestMessage) {
-	let newMessage = {};
+  let newMessage = {};
 
-	if (oldRequestMessage) {
-		let infoCopy = cloneDeep(oldRequestMessage.info);
-		let hopsCopy = cloneDeep(oldRequestMessage.hops);
-		let newInfoCopy = cloneDeep(newData.info);
+  if (oldRequestMessage) {
+    let infoCopy = cloneDeep(oldRequestMessage.info);
+    let hopsCopy = cloneDeep(oldRequestMessage.hops);
+    let newInfoCopy = cloneDeep(newData.info);
 
-		newMessage = {
-			info: infoCopy,
-			hops: hopsCopy
-		};
+    newMessage = {
+      info: infoCopy,
+      hops: hopsCopy
+    };
 
-		merge(newMessage.info, newInfoCopy);
-		newMessage.payload = newData.payload || {};
-	} else if (newData) {
-		let newInfoCopy = cloneDeep(newData.info);
-		if (newData.hops) {
-			newMessage.hops = cloneDeep(newData.hops);
-		} else {
-			newMessage.hops = [];
-		}
-		newMessage.info = newInfoCopy;
-		newMessage.payload = newData.payload || {};
-	} else {
-		newMessage = {
-			info: {},
-			hops: [],
-			payload: {}
-		};
-	}
+    merge(newMessage.info, newInfoCopy);
+    newMessage.payload = newData.payload || {};
+  } else if (newData) {
+    let newInfoCopy = cloneDeep(newData.info);
+    if (newData.hops) {
+      newMessage.hops = cloneDeep(newData.hops);
+    } else {
+      newMessage.hops = [];
+    }
+    newMessage.info = newInfoCopy;
+    newMessage.payload = newData.payload || {};
+  } else {
+    newMessage = {
+      info: {},
+      hops: [],
+      payload: {}
+    };
+  }
 
-	return newMessage;
+  return newMessage;
 }
 
 /**
@@ -57,12 +57,12 @@ export function createMessage(newData, oldRequestMessage) {
  * @param endpoint {string} The name of endpoint the message is routed through
  */
 export function addHop(message, stepName, stepType, endpoint) {
-	message.hops.push({
-		time: Date.now(),
-		id: uuid.create(4).toString(),
-		stepName: stepName,
-		stepType: stepType,
-		endpoint: endpoint,
-		host: os.hostname()
-	});
+  message.hops.push({
+    time: Date.now(),
+    id: uuid.create(4).toString(),
+    stepName: stepName,
+    stepType: stepType,
+    endpoint: endpoint,
+    host: hostname()
+  });
 }
--- tests/message-test.mjs
+++ tests/message-test.mjs
@@ -1,7 +1,3 @@
-/*global describe, it*/
-/* jslint node: true, esnext: true */
-'use strict';
-
 const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
